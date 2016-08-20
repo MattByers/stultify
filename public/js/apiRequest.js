f05d1552bdce73df3d1e6d2604fdf332234bd2ea
AhmedShab <ahmed.vuw@gmail.com>
2016-08-21 09:18:29 +1200

    ready to get data from the backend

--- public/js/apiRequest.js
+++ public/js/apiRequest.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var url = "http://api.openweathermap.org/data/2.5/forecast?q=Wellington,nz&appid=155a3b1ea43baad50689bcce9ae5629e";
+  var url = "/api/events";
   $.get(url, function(data, status){
         console.log("data", data);
     });
