8fe66167776fe86cef11ec238fe04776307613c9
Peter SCriven <justabitdope@gmail.com>
2016-08-21 09:17:03 +1200

    Small map changes

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   // Initialize Map
-  var map = L.map('mapid').setView([-41.286, 174.77], 13);
+  var map = L.map('mapid', // Map Options 
+                       { center: [-41.286, 174.77],
+                         zoom: 13
+                       }
+    );
 
   // Tiles
   L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoianVzdGFiaXRkb3BlIiwiYSI6ImNpczJkcjZ5MTAwaGQydHAxNmN2ZHh1NWUifQ.CwwXo2NNcUduhDqbN8tmNA', 
@@ -12,5 +16,17 @@ $(document).ready(function() {
     }
   ).addTo(map);
 
+  // Marker Icon
+  var myIcon = L.divIcon({className: 'my-div-icon', iconSize: 200});
+
+  // Add a test token
+  var marker = L.marker([-41.286, 174.77],
+    // Marker Options
+    { icon: myIcon
+    }
+    ).addTo(map);
+
+  // Test popup
+  marker.bindPopup('<p><b>Hello world!</b><br />This is a nice popup.</p>')
 
 });
