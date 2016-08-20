29082fd97c74433f487a52322de549c6bfc8f903
JakeDorne <jakedorne@hotmail.com>
2016-08-20 16:38:02 +1200

    Can get events within radius of loc and call from other files

--- exports/eventfinda.js
+++ exports/eventfinda.js
@@ -2,48 +2,37 @@ var express = require('express');
 var router = express.Router();
 var unirest = require('unirest');
 var distance = require('gps-distance');
-//var exports = module.exports = {}
-var events;
+var exports = module.exports = {}
 
-var req = //function(){
+exports.getEvents = function(callback){
   unirest.get("https://eventfinda-eventfinda-nz.p.mashape.com/events.json")
   .header("Authorization", "Basic bG9jYWxob3N0Mjp0cWNrbmpja2doYnY=")
   .header("X-Mashape-Key", "Lv4F833PBpmshgpogHNJQN98NwKap12ojv9jsn6t5pzZypLeKh")
   .header("Accept", "application/json")
   .end(function (result) {
-    events = result.body.events;
+    callback(result.body.events);
   });
-  return events;
-// }
+}
 
-function getEventsAroundPoint(lat, lon, rad) {
+exports.getEventsAroundPoint = function(lat, lon, rad, events, callback) {
   toReturn = [];
-  for(i = 0; i < events.length; i++){
+  for(i = 0; i < eventArray.length; i++){
     if(distance(lat, lon, events[i].point.lat, events[i].point.lng) < rad){
       toReturn.push(events[i]);
     }
   }
-  return toReturn;
+  callback(toReturn);
 }
 
-function printEvents(events){
+exports.printEvents = function(events){
   for(i = 0; i < events.length; i++){
     console.log(events[i].name);
   }
 }
 
-function printAllEvents(){
-  for(i = 0; i < events.length; i++){
-    console.log(events[i]);
-  }
-}
-
-function findParents(){
+exports.findAllParents = function(events){
   for(i = 0; i < events.length; i++){
     console.log("id" + i + " :" + events[i].category.parent_id);
     console.log("name" + i + " :" + events[i].category.name);
-
   }
 }
-
-module.exports = router;
