3cb34240cd462681dd2b7e3a48396a1eb05c66f1
AhmedShab <ahmed.vuw@gmail.com>
2016-08-21 15:01:43 +1200

    added fb and ef logos

--- public/js/map.js
+++ public/js/map.js
@@ -1,71 +1,91 @@
 $(document).ready(function() {
 
-    // Initialize Map
-    var map = L.map('mapid', // Map Options
-                         { center: [-41.286, 174.77],
-                           zoom: 13
-                         }
-      );
+  var lat = -41.286;
+  var long = 174.77;
 
-    // Tiles
-    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoianVzdGFiaXRkb3BlIiwiYSI6ImNpczJkcjZ5MTAwaGQydHAxNmN2ZHh1NWUifQ.CwwXo2NNcUduhDqbN8tmNA',
-      {
-        attribution: 'Hello',
-        maxZoom: 18,
-        id: 'mapbox.mapbox-streets-v7',
-        accessToken: 'pk.eyJ1IjoianVzdGFiaXRkb3BlIiwiYSI6ImNpczJkcjZ5MTAwaGQydHAxNmN2ZHh1NWUifQ.CwwXo2NNcUduhDqbN8tmNA' // Peter's Token
-      }
-    ).addTo(map);
+  // Initialize Map
+  var map = L.map('mapid', // Map Options
+  { center: [lat, long],
+    zoom: 13,
+    zoomControl: false
+  }
+);
 
-    // Marker Icon
-    var myIcon = L.divIcon({className: 'my-div-icon', iconSize: 200});
+  // Tiles
+  L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoianVzdGFiaXRkb3BlIiwiYSI6ImNpczJkcjZ5MTAwaGQydHAxNmN2ZHh1NWUifQ.CwwXo2NNcUduhDqbN8tmNA',
+  {
+    attribution: 'Hello',
+    maxZoom: 18,
+    id: 'mapbox.mapbox-streets-v7',
+    accessToken: 'pk.eyJ1IjoianVzdGFiaXRkb3BlIiwiYSI6ImNpczJkcjZ5MTAwaGQydHAxNmN2ZHh1NWUifQ.CwwXo2NNcUduhDqbN8tmNA' // Peter's Token
+  }).addTo(map);
 
-    // Add a test token
-    var marker = L.marker([-41.286, 174.77]
-      // Marker Options
-      // { icon: myIcon
-      // }
-      ).addTo(map);
+getData(lat, long);
 
-    // Test popup
-    marker.bindPopup('<p><b>Hello world!</b><br />This is a nice popup.</p>');
+//get user postion
+map.on('moveend', function(e) {
+  var lat = map.getCenter().lat;
+  var long = map.getCenter().lng;
+  getData(lat, long);
+});
 
+function getData(lat, long) {
+  console.log("before calling the api");
+  var url = "api/events/"+ lat +'/'+ long;
 
-    var url = "api/events";
-
-    // making api get request
-    $.get(url, function(data, status){
-      placeOnMap(data);
-    });
-
-    function placeOnMap(data) {
-      data.map(addToMap);
-    }
-
-    function addToMap(eventDetail) {
-      // console.log("looping through the data");
-      // Tiles
-      L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoianVzdGFiaXRkb3BlIiwiYSI6ImNpczJkcjZ5MTAwaGQydHAxNmN2ZHh1NWUifQ.CwwXo2NNcUduhDqbN8tmNA',
-        {
-          attribution: 'Hello',
-          maxZoom: 18,
-          id: 'mapbox.mapbox-streets-v7',
-          accessToken: 'pk.eyJ1IjoianVzdGFiaXRkb3BlIiwiYSI6ImNpczJkcjZ5MTAwaGQydHAxNmN2ZHh1NWUifQ.CwwXo2NNcUduhDqbN8tmNA' // Peter's Token
-        }
-      ).addTo(map);
-
-      // Marker Icon
-      var myIcon = L.divIcon({className: 'my-div-icon', iconSize: 200});
-
-      // Add a test token
-      var marker = L.marker([eventDetail.lat, eventDetail.long]
-        // Marker Options
-        // { icon: myIcon
-        // }
-        ).addTo(map);
-
-      // Test popup
-      marker.bindPopup('<p><b>' + eventDetail.name + '</b><br />' + eventDetail.description + '</p>');
-    }
-
+  // making api get request
+  $.get(url, function(data, status){
+    console.log("got data");
+    placeOnMap(data);
   });
+
+  function placeOnMap(data) {
+    data.map(addToMap);
+  }
+
+  function addToMap(eventDetail) {
+
+  // Marker Icon
+  var fbIcon = L.icon({
+    iconUrl: '/images/fb_icon.png',
+
+    iconSize:     [38, 38], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+  // ef_icon.png
+  var efIcon = L.icon({
+    iconUrl: '/images/ef_icon.png',
+
+    iconSize:     [38, 38], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+
+  var myIcon;
+  if (eventDetail.source === "facebook") {
+    myIcon = fbIcon;
+  }
+  else {
+    myIcon = efIcon;
+  }
+
+  // Add a test token
+  var marker = L.marker([eventDetail.lat, eventDetail.long],
+    // Marker Options
+    {icon: myIcon}
+  ).addTo(map);
+
+  // Test popup
+  marker.bindPopup('<p><b>' + eventDetail.name + '</b><br />' + eventDetail.description + '</p>');
+}
+}
+
+console.log("after calling the api");
+});
